19077bb40aab730f1fa72531fedb2ca17d9801a0
Dante <hubert.dega@gmail.com>
2021-03-17 21:38:06 +0100

    adding folders

---
+++ Assets/Animation/AnimatorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorScript : MonoBehaviour
+{
+    [SerializeField] private Sprite[] frameArray;
+    private int currentFrame;
+    private float timer;
+    public float frameRate;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= frameRate)
+        {
+            timer -= frameRate;
+            currentFrame = (currentFrame + 1) % frameArray.Length;
+            spriteRenderer.sprite = frameArray[currentFrame];
+        }
+    }
+}
---
+++ Assets/LevelInfinite.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelInfinite : MonoBehaviour
+{
+    public GameObject ball1;
+    public GameObject ball2;
+    public Transform pos1;
+    public Transform pos2;
+
+    public GameHandler gameHandler;
+
+    private int rand;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] objectsCount = GameObject.FindGameObjectsWithTag("Ball");
+        if (objectsCount.Length < 3)
+        {
+            rand = Random.Range(0, 2);
+            if(rand == 0)
+            {
+                rand = Random.Range(0, 2);
+                if(rand == 0)
+                {
+                    GameObject Ball1 = Instantiate(ball1, pos1.position, Quaternion.identity);
+                    Ball1.GetComponent<Ball>().startForce = new Vector2(2f, 1f);
+                    gameHandler.GetComponent<GameHandler>().addTime();
+                }
+                else
+                {
+                    GameObject Ball2 = Instantiate(ball2, pos1.position, Quaternion.identity);
+                    Ball2.GetComponent<Ball>().startForce = new Vector2(2f, 1f);
+                    gameHandler.GetComponent<GameHandler>().addTime();
+                }
+            }
+            else
+            {
+                rand = Random.Range(0, 2);
+                if (rand == 0)
+                {
+                    GameObject Ball3 = Instantiate(ball1, pos2.position, Quaternion.identity);
+                    Ball3.GetComponent<Ball>().startForce = new Vector2(-2f, 1f);
+                    gameHandler.GetComponent<GameHandler>().addTime();
+                }
+                else
+                {
+                    GameObject Ball4 = Instantiate(ball2, pos2.position, Quaternion.identity);
+                    Ball4.GetComponent<Ball>().startForce = new Vector2(-2f, 1f);
+                    gameHandler.GetComponent<GameHandler>().addTime();
+                }
+            }
+            
+        }
+    }
+}
